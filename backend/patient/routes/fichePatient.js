115fa90753d23a55450a81c8acb83a669770ad18
belcaid abderrahim <belcaidna@gmail.com>
2021-01-24 23:51:36 +0100

    merge question branch

--- backend/patient/routes/fichePatient.js
+++ backend/patient/routes/fichePatient.js
@@ -9,18 +9,16 @@ router.route("/").get((req, res) => {
 
 
 router.route("/update/:id").put((req, res) => {
-  const idPatient = req.body.idPatient;
-  const dateTest = req.body.dateTest;
+  // const idPatient = req.body.idPatient;
+  // const dateTest = req.body.dateTest;
   const rsTest = req.body.rsTest;
   
 
-  const fichePatientUpdate = new FichePatient({
-    _id: req.params.id,
-    idPatient,
-    dateTest,
-    rsTest
-  });
-  FichePatient.updateOne({_id: req.params.id}, fichePatientUpdate)
+  // const fichePatientUpdate = new FichePatient({
+  //   idPatient: req.params.id,
+  //   rsTest
+  // });
+  FichePatient.updateOne({idPatient: req.params.id}, {rsTest : rsTest})
   .then(() => res.status(201).json("fiche Patient successfully update"))
   .catch((err) => res.status(400).json("Error :" + err));
 })
@@ -43,8 +41,14 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error :" + err));
 });
 
-router.route("/:id").get((req, res) => {
-  FichePatient.findById(req.params.id)
+// router.route("/:id").get((req, res) => {
+//   FichePatient.findById(req.params.id)
+//     .then((fichePatient) => res.json(fichePatient))
+//     .catch((err) => res.status(400).json("Error :" + err));
+// });
+
+router.route("/:dossierId").get((req, res) => {
+  FichePatient.findOne({idPatient: `${req.params.dossierId}`})
     .then((fichePatient) => res.json(fichePatient))
     .catch((err) => res.status(400).json("Error :" + err));
 });
--- backend/patient/server.js
+++ backend/patient/server.js
@@ -14,7 +14,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
